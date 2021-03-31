0c0b0d3ce1005a88c2fbedd104941866957a0ea9
thomaskost17 <thomaskost17@ucla.edu>
2021-03-30 19:05:14 -0700

    basic reciever implementation and PAL initialization

---
+++ PAL/README.md
@@ -0,0 +1,3 @@
+# PAL
+
+**P**hotographic **A**ltitude and optical f**L**ow sensor (PAL) is a Raspberry Pi zero W based optical flow sensor that makes use of a logitec webcam and an accelerometer. Its goal is to provide through I2C connection to a teensy 4.1 altitude and horizontal planar displacement information. 
---
+++ PAL/src/main.cpp
@@ -0,0 +1,26 @@
+/**
+ *  File: main.cpp
+ * 
+ *  Author: Thomas Kost
+ *  
+ *  Date: 30 March 2021
+ *  
+ *  @brief this is the optical flow sensor main file for PAL on LASSIE. This is intended to be run on ARM
+ *         architecture of a raspberry pi
+ *
+ */
+
+#include <wiringPi.h>
+
+int main(int argc, char *argv[]){
+
+    // hardware setup
+    wiringPiSetup();
+
+    // Initialize camera connection
+
+    // Initialize I2C connection
+
+    // Read Image,read accelerometer, Process info, provide if data available-- loop
+    return 0;
+}
